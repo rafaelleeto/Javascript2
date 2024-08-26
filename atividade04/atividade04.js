9503cc9e02696026327aa68405077b8f77493c6b
rafaelleeto <leetobr@gmail.com>
2024-08-26 17:14:32 -0300

    IMC CALCULADORA PRONTA

--- atividade04/atividade04.js
+++ atividade04/atividade04.js
@@ -5,7 +5,7 @@ function calcular(){
     var altura2= altura*altura
     var resultado= peso/altura2
     
-    document.getElementById("resultado").textContent= "seu IMC É:" + resultado
+    document.getElementById("resultado").textContent= "seu IMC É:" + resultado.toFixed(2)
 }
 
     
